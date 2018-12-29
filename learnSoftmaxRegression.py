a819a9d89e7dac5588e7532e19b4de9042c3f38c
JR <jiangrong1994@yeah.net>
2018-12-29 16:47:32 +0800

    create learnSoftmaxRegression.py

---
+++ learnSoftmaxRegression.py
@@ -0,0 +1,6 @@
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print(mnist.train.images.shape, mnist.train.labels.shape)
+print(mnist.test.images.shape, mnist.test.labels.shape)
+print(mnist.validation.images.shape, mnist.validation.labels.shape)
